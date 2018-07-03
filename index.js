daf2d39f7e6566d198b9f6d79d3891a6c94e1cec
arianna carter <carterari43@gmail.com>
2018-07-03 18:35:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function dwarfRollCall(dwarves) {
+<<<<<<< HEAD
   var returnStr = [];
   for (var i = 0 ; i < dwarves.length; i ++){
     returnStr.push( `${i +1}. ${dwarves[i]} ` )
+=======
+  var returnStr = ""; 
+  for (var i = 0 ; i > dwarves.length; i ++){
+    returnStr.push( ` ${i +1}'.' + ${dwarves[i]} ` )
+>>>>>>> 04b7d121c69f668b952288c0ae467e01e82b9a67
   }
   return returnStr.join('');
 }
@@ -28,6 +34,7 @@ function longPlaneteerCalls(words) {
 function findTheCheese (foods) {
   var cheese = ["cheddar", "gouda", "camembert"];
   for (var i = 0; i < foods.length; i++ ){
+<<<<<<< HEAD
   	if ( foods[i] === "cheddar"||foods[i] ==="gouda" ||foods[i] ==="camedbert"){
   		return foods[i]
   	} 
@@ -35,3 +42,13 @@ function findTheCheese (foods) {
   		return "no cheese!"
   
 }
+=======
+  	if ( foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camedbert"){
+  		return foods[i]
+  	} else {
+  		return "no cheese!"
+  	}
+   
+  }
+}
+>>>>>>> 04b7d121c69f668b952288c0ae467e01e82b9a67
